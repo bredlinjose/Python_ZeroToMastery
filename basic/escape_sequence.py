6fc06927fcfa31aaa3d6d3072ab290d90609bc96
Bredlin <bredlinjose@gmail.com>
2024-06-26 21:38:19 +0530

    python basic

--- basic/escape_sequence.py
+++ basic/escape_sequence.py
@@ -1,5 +1,5 @@
 print("It's sunny")
-print("It\'s sunny")  # \ represent next is symbol
+print("It\'s sunny")  # \ represent next is symbol.
 print("It's a \"kind of sunny\"")
 print("\tIt's sunny")  # \t --> tab
-print("It's rainy \nTake umberlla")  # \n --> next line
+print("It's rainy \nTake umbrella")  # \n --> next line
